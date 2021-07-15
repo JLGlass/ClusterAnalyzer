0e813753bb21b30767e939c3908f2b1402e39fc1
Ethan Braunstein <braunstein.ethan@gmail.com>
2021-07-15 13:57:45 -0400

    Added basic Plotly functionality to app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import re
+import plotly.graph_objects as go
 
 def removeNewLine(lst_file):
     lst_fileClean = []
@@ -39,6 +40,14 @@ def findFragments(int_fragmentRange, lst_regExPos):
         int_pos2 += 1
     return lst_fragmentSize
 
+#Plotly
+def makePlot():
+    fig = go.Figure()
+    for i in range(1000):
+        fig.add_trace(go.Box(x = (i,lst_fragments[i]), name = f"Fragment {i}", marker_color = "black"))
+    fig.update_layout(title = f"Fragment Range: {int_fragmentRange}")
+    fig.show()
+
 #Open file
 filePath = "E:\Internship_MSK\chr21.fa"
 file = open(filePath, "r")
@@ -46,7 +55,7 @@ lst_file = file.readlines()
 
 #Define variables
 str_pattern = str(input("Regular expression to search for: "))
-int_fragmentRange = int(input("Size of fragments: "))
+int_fragmentRange = int(input("Range of fragments: "))
 
 #Run code
 lst_fileClean = removeNewLine(lst_file)
@@ -58,4 +67,6 @@ lst_fragments = findFragments(int_fragmentRange, lst_regExPos)
 print(f"The headers are located at lines: {dct_headers}")
 print(f"There are {len(lst_regExPos)} {str_pattern} positions.")
 print(f"The first 5 are {lst_regExPos[0:4]} and the last 5 are {lst_regExPos[len(lst_regExPos)-5:len(lst_regExPos)]}")
-print(f"The fragments are: {lst_fragments}")
+print(f"The fragments are: {lst_fragments[0:100]}")
+
+makePlot()
